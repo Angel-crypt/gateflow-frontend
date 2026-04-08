52a8e5eb06fff37b6f018ca22ae16cf0fccb1f94
Angel-crypt <angel.cruz.030905@gmail.com>
2026-04-08 17:34:20 -0600

    fix(config): refactor Vite configuration to support HTTPS based on environment variable

--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,6 @@ import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import basicSsl from '@vitejs/plugin-basic-ssl'
 
-<<<<<<< HEAD
-export default defineConfig({
-  plugins: [react(), basicSsl()],
-  server: {
-    proxy: {
-      '/auth': {
-        target: 'http://192.168.59.243:8000',
-        changeOrigin: true,
-        rewrite: (path) => `/api${path}`,
-      },
-      '/api': {
-        target: 'http://192.168.59.243:8000',
-        changeOrigin: true,
-      },
-=======
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '')
   const useHttps = env.VITE_DEV_HTTPS === 'true'
@@ -26,7 +11,6 @@ export default defineConfig(({ mode }) => {
     server: {
       https: useHttps,
       host: true,
->>>>>>> 9133937 (fix(config): refactor Vite configuration to support HTTPS based on environment variable)
     },
   }
 })
